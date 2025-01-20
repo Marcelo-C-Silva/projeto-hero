d2425520e5067f4380d0481227c51eeac7615d44
marcelo <carneiromarcelodev@gmail.com>
2025-01-20 11:37:14 -0300

    adiciona AppRoutingModule ao módulo principal e remove importação desnecessária

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
